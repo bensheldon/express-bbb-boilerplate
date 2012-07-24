3f4494a5d80f803ad0705b17a5ac3775656078f3
Ben Sheldon <bensheldon@gmail.com>
2012-07-24 09:59:29 -0700

    Added Backbone-Boilerplate and moved assets around to accommodate it; moved to EJS for templating (which isn't the same thing as backbone's underscore templates, but it's close enough… for now)

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function(req, res) {
-  res.render('index', { 
-    title: "Boilerplate"
+  res.render('index', {
+    title: "Boilerplate",
+    layout : false
   });
 }
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 3000;
 // Express Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
